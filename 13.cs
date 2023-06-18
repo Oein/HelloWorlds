1033ad6ceeb33b287c8e18fcd8542e7f7801700c
Oein <oein0219@gmail.com>
2023-06-18 21:55:57 +0900

    add 37 things

---
+++ 13.cs
@@ -0,0 +1,9 @@
+namespace Main
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 32.c
@@ -0,0 +1,4 @@
+#include <bits/stdc++.h>
+int main() {
+    puts("Hello World!");
+}
